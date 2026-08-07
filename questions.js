0e08da0c9e3fcd50267515dab3dafa8795ad76ac
annocuoi <daikatai2013@gmail.com>
2026-08-07 19:00:13 +0700

    update câu hỏi mới

--- questions.js
+++ questions.js
@@ -4,12 +4,12 @@ const questions = [
     { q: "2 + 3 * 2 bằng mấy?", a: "8" },
     { q: "Con vật nào được mệnh danh là chúa sơn lâm?", a: "hổ" },
     { q: "Trái Đất quay quanh mặt trời mất bao nhiêu ngày?", a: "365" },
-    { q: "Đâu là ngôn ngữ lập trình phổ biến cho AI?", a: "python" },
+    { q: "Ai xui nhất ?", a: "Con Tuyết" },
     { q: "Nước nào có hình dáng giống chiếc ủng?", a: "ý" },
-    { q: "Đại dương nào lớn nhất thế giới?", a: "thai binh duong" },
-    { q: "Hành tinh nào gần Mặt Trời nhất?", a: "sao thuy" },
-    { q: "1 kg sắt và 1 kg bông, cái nào nặng hơn?", a: "bang nhau" },
-    { q: "Màu nào khi pha giữa đỏ và vàng?", a: "cam" }
+    { q: "Đại dương nào lớn nhất thế giới?", a: "Thái Bình Dương" },
+    { q: "Ai Đẹp Trai Nhất Thế Giới ?", a: "Tài Gió" },
+    { q: "Ai đáng bị đuổi nhất ở Hội Độ Kiếp?", a: "Chị Bình" },
+    { q: "Màu nào khi pha giữa đỏ và vàng?", a: "Cam" }
 ];
 
 // Xuất ra để file server.js có thể dùng được
